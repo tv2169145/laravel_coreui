9ae6b37f2a4d1607a68e1cdc0edbc7c2a723a372
zhean <tv2169145@gmail.com>
2020-09-20 14:24:42 +0800

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        {{ msg }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "hello jimmy",
+            };
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
