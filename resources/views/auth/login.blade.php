ec508923e13b50b150a822f75b643b10dbc94107
Jimmy_Lin <jimmy_lin@talfin.ai>
2020-09-21 14:17:54 +0800

    edit again again again

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,17 +43,17 @@
                     </div>
               </div>
             </div>
-            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-              <div class="card-body text-center">
-                <div>
-                  <h2>Sign up</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  @if (Route::has('password.request'))
-                    <a href="{{ route('register') }}" class="btn btn-primary active mt-3">{{ __('Register') }}</a>
-                  @endif
-                </div>
-              </div>
-            </div>
+{{--            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">--}}
+{{--              <div class="card-body text-center">--}}
+{{--                <div>--}}
+{{--                  <h2>Sign up</h2>--}}
+{{--                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>--}}
+{{--                  @if (Route::has('password.request'))--}}
+{{--                    <a href="{{ route('register') }}" class="btn btn-primary active mt-3">{{ __('Register') }}</a>--}}
+{{--                  @endif--}}
+{{--                </div>--}}
+{{--              </div>--}}
+{{--            </div>--}}
           </div>
         </div>
       </div>
